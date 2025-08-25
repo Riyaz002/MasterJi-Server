1423af1a0f7009e9762ecf823ebb5f6b83d0987c
Riyaz <riyazps007@gmail.com>
2025-08-25 21:10:57 +0530

    ref: created user schema

---
+++ src/api/models/user.js
@@ -0,0 +1,39 @@
+// user.js
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "Basic",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const User = mongoose.model("user", UserSchema)
+module.exports = User
